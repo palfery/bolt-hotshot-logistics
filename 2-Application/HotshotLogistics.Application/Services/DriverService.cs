19f9193c416fac4d120da5c943f8687a717ce614
dpalfery <dave@palfery.com>
2025-06-08 23:42:01 -0400

    cleaned up following the Clean Architecture pattern for C# projects

---
+++ 2-Application/HotshotLogistics.Application/Services/DriverService.cs
@@ -0,0 +1,33 @@
+using HotshotLogistics.Contracts.Models;
+using HotshotLogistics.Contracts.Services;
+using HotshotLogistics.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotshotLogistics.Application.Services
+{
+    public class DriverService : IDriverService
+    {
+        private readonly IDriverRepository _driverRepo;
+
+        public DriverService(IDriverRepository driverRepo)
+        {
+            _driverRepo = driverRepo;   
+        }
+        public Task<IDriver> CreateDriverAsync(IDriver driver)
+        {
+            return _driverRepo.CreateDriverAsync(driver);
+        }
+
+        public Task<IDriver?> GetDriverByIdAsync(int id)
+        {
+            return _driverRepo.GetDriverByIdAsync(id);
+        }
+
+        public Task<IEnumerable<IDriver>> GetDriversAsync()
+        {
+            return _driverRepo.GetDriversAsync();
+        }
+    }
+}
