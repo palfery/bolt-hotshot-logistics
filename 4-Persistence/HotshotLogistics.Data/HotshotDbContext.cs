695b02310c70df9490887d0f5e7e758ef580af9d
David Palfery <davidpalfery@gmail.com>
2025-06-10 23:25:22 -0400

    Upgrade projects to .NET 8 and fix driver repo

--- 4-Persistence/HotshotLogistics.Data/HotshotDbContext.cs
+++ 4-Persistence/HotshotLogistics.Data/HotshotDbContext.cs
@@ -11,7 +11,7 @@ public class HotshotDbContext : DbContext
     {
     }
 
-    public DbSet<IDriver> Drivers { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
